b4fdece36eeaadbc0d6ceeb8a1605d35da0254da
Yel69-420 <magsinoariel@gmail.com>
2024-10-24 18:01:08 +0800

    remove ignored folders

--- node_modules/@ngrx/eslint-plugin/src/utils/helper-functions/docs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.docsUrl = void 0;
-var docsUrl = function (ruleName) {
-    return "https://ngrx.io/guide/eslint-plugin/rules/".concat(ruleName);
-};
-exports.docsUrl = docsUrl;
-//# sourceMappingURL=docs.js.map
