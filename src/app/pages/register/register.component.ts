ab940af8854b50a822c1f5e6a1ef5ec102c748d2
Yel69-420 <magsinoariel@gmail.com>
2024-10-26 15:17:51 +0800

    update signup page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {
   FormBuilder,
@@ -7,10 +7,12 @@ import {
   FormControl,
 } from '@angular/forms';
 import { first } from 'rxjs/operators';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faL, faUser } from '@fortawesome/free-solid-svg-icons';
 
 import { AuthenticationService } from '@core/auth';
 import { MessageService } from 'primeng/api';
+import { ConfirmPasswordValidator } from '@app/core/validators/confirm-password.validator';
+import { HttpService } from 'src/services/http.service';
 
 @Component({
   templateUrl: 'register.component.html',
@@ -28,15 +30,22 @@ export class RegisterComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private httpSvc: HttpService
   ) {
    
     this.registerForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required],
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      phone: ['', Validators.required]
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/)]],
+      confirmPassword: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.pattern(/[\S]/)]],
+      lastname: ['', [Validators.required, Validators.pattern(/[\S]/)]],
+      phone: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      isPwd: false,
+      isSenior: false,
+      isPregnant: false
     });
   }
 
@@ -56,9 +65,90 @@ export class RegisterComponent implements OnInit {
 
     // stop here if form is invalid
     if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched;
       return;
     }
 
     this.loading = true;
+
+    this.httpSvc.post("Authenticate/Register", this.registerForm.getRawValue()).subscribe(
+      response => {
+        this.messageService.add({
+          severity: response.status.toLowerCase(),
+          summary: 'Sign up completed',
+          detail: response.message,
+        });
+
+        this.router.navigate(['']);
+      },
+      error => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Save Record',
+          detail: error.error.message,
+        });
+      }
+    );
+  }
+
+  validatePassword() {
+    console.log("validate")
+    if (
+      this.registerForm.get('password')?.value ||
+      this.registerForm.get('confirmPassword')?.value
+    ) {
+      this.registerForm
+        .get('password')
+        ?.setValidators([
+          Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/),
+        ]);
+      this.registerForm.addValidators(
+        ConfirmPasswordValidator(
+          this.registerForm.get('password'),
+          this.registerForm.get('confirmPassword')
+        )
+      );
+
+      //workaround for not working validator, need to find other way
+      if (this.registerForm.get('password')?.value.length < 8) {
+        this.registerForm.get('password')?.setErrors({ pattern: true });
+      }
+    } else {
+      this.registerForm.get('password')?.setValidators([
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/),
+        Validators.required
+      ]);
+      this.registerForm.get('password')?.setErrors({required: true});
+      this.registerForm.addValidators(
+        ConfirmPasswordValidator(
+          this.registerForm.get('password'),
+          this.registerForm.get('confirmPassword')
+        )
+      );
+      if(this.registerForm.get('password')?.value.length > 0 && 
+      this.registerForm.get('password')?.value != this.registerForm.get('confirmPassword')?.value ) {
+        this.registerForm.get('confirmPassword')?.setErrors({confirmPasswordValidator: true});
+      }
+      else {
+        this.registerForm.get('confirmPassword')?.setErrors({required: true});
+      }
+    }
+
+    this.changeDetectorRef.detectChanges();
+    this.registerForm.get('confirmPassword')?.markAsTouched;
+    this.registerForm.get('password')?.markAsTouched;
+
+    this.registerForm.updateValueAndValidity();
+  }
+
+  validateControl(controlName: string): boolean {
+    if (
+      (this.registerForm.get(controlName)?.dirty ||
+        this.registerForm.get(controlName)?.touched) &&
+      this.registerForm.get(controlName)?.invalid
+    ) {
+      return true;
+    }
+    return false;
   }
 }
